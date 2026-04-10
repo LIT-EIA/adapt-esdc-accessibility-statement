f532721da3c03d863b11b7595f6e2b56bff48f68
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2026-04-10 13:13:36 -0400

    Updated statement

--- assets/Adapt-accessibilitystatement.js
+++ assets/Adapt-accessibilitystatement.js
@@ -22,8 +22,9 @@ if (statementConfigs._statementEnabled) {
       }
       var statementBody = {
         part1: $.i18n.translate('adapt-accessibility-statement-body-1'),
-        customPart: statementConfigs._customSection,
         part2: $.i18n.translate('adapt-accessibility-statement-body-2'),
+        customPart: statementConfigs._customSection,
+        part3: $.i18n.translate('adapt-accessibility-statement-body-3'),
         dontShow: $.i18n.translate('adapt-accessibility-statement-dont-show'),
         simulation: simulation !== undefined,
         simulationStatement: $.i18n.translate('adapt-accessibility-statement-simulation')
@@ -51,16 +52,18 @@ if (statementConfigs._statementEnabled) {
   $.i18n().load({
     'en': {
       'adapt-accessibility-statement-title': 'Accessibility Statement',
-      'adapt-accessibility-statement-body-1': 'The College@ESDC is committed to digital accessibility for all ESDC employees with consideration for the diverse needs of learners in our e-learning products. We are continually improving the user experience and applying the relevant accessibility standards to the best of our abilities and resources available. The Web Content Accessibility Guidelines (WCAG) define requirements for designers and developers to improve accessibility.  The College@ESDC is striving to create accessible courses to conform with WCAG 2.1 level AA (and our internal best practices).',
-      'adapt-accessibility-statement-body-2': `If you encounter an issue, please contact us via the email field on our <a href="${statementConfigs._sabaProfile}" target="_blank">Saba Profile Page</a>.`,
+      'adapt-accessibility-statement-body-1': 'The College@ESDC is committed to providing accessible, barrier-free learning for all employees with consideration for the diverse needs of learners in our e-learning products. We are continuously improving the user experience and applying the relevant accessibility standards to the best of our abilities and resources available.',
+      'adapt-accessibility-statement-body-2': 'The CAN/ASC – EN 301 549:2024 standard, which includes Web Content Accessibility Guidelines (WCAG) 2.1 level AA requirements, defines requirements for designers and developers to improve accessibility. We strive to meet these standards across all platforms and content.',
+      'adapt-accessibility-statement-body-3': `If you encounter an issue, please contact us via the email field on our <a href="${statementConfigs._sabaProfile}" target="_blank">Saba Profile Page</a>.`,
       'adapt-accessibility-statement-dont-show': 'Don\'t show again until page refresh',
       'adapt-accessibility-statement-close': 'Close',
       'adapt-accessibility-statement-simulation': 'This course includes simulation activities that could have minor accessibility limitations. These simulations focus on key learning points and may not include all features of the original systems. Simulations may reflect certain accessibility barriers present in the original system.'
     },
     'fr': {
       'adapt-accessibility-statement-title': 'Énoncé d\'accessibilité',
-      'adapt-accessibility-statement-body-1': 'Le Collège@EDSC s\'engage à assurer l\'accessibilité numérique pour tous les employés d\'EDSC en tenant compte des divers besoins des apprenants qui utilisent nos produits d\'apprentissage en ligne. Nous améliorons continuellement l\'expérience utilisateur et appliquons les normes d\'accessibilité applicables au meilleur de nos capacités et des ressources disponibles. Les Règles pour l\'accessibilité des contenus Web (WCAG) définissent les exigences pour les concepteurs et les développeurs afin d\'améliorer l\'accessibilité. Le College@EDSC s\'efforce de créer des cours accessibles pour se conformer aux Règles pour l\'accessibilité des contenus Web (WCAG) 2.1 niveau AA (et à nos meilleures pratiques internes).',
-      'adapt-accessibility-statement-body-2': `Si vous rencontrez un problème, veuillez nous contacter via le champ courriel sur notre <a href="${statementConfigs._sabaProfile}" target="_blank">Page de Profil Saba</a>.`,
+      'adapt-accessibility-statement-body-1': 'Le Collège@EDSC s’engage à offrir un apprentissage accessible et sans obstacles à tous les employés, en tenant compte de la diversité des besoins des apprenants dans ses produits d’apprentissage en ligne. Nous travaillons continuellement à améliorer l’expérience utilisateur et à appliquer, dans la mesure de nos capacités et des ressources disponibles, les normes d’accessibilité pertinentes.',
+      'adapt-accessibility-statement-body-2': 'La norme CAN/ASC – EN 301 549:2024, qui intègre les exigences de niveau AA des Règles pour l’accessibilité des contenus Web (WCAG) 2.1, établit des exigences à l’intention des concepteurs et des développeurs afin d’améliorer l’accessibilité. Nous nous efforçons de respecter ces exigences sur l’ensemble de nos plateformes et de nos contenus.',
+      'adapt-accessibility-statement-body-3': `Si vous rencontrez un problème, veuillez nous contacter via le champ courriel sur notre <a href="${statementConfigs._sabaProfile}" target="_blank">Page de Profil Saba</a>.`,
       'adapt-accessibility-statement-dont-show': 'Ne pas afficher de nouveau jusqu\'à la prochaine actualisation de la page',
       'adapt-accessibility-statement-close': 'Fermer',
       'adapt-accessibility-statement-simulation': 'Ce cours comprend des activités de simulation qui peuvent présenter des limitations mineures en matière d\'accessibilité. Ces simulations ciblent des points clés de l\'apprentissage et peuvent ne pas inclure toutes les caractéristiques des systèmes originaux. Les simulations peuvent refléter certains obstacles à l\'accessibilité présents dans le système d\'origine.'
